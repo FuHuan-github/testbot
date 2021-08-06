58925680a40fb9781595da7819d4a788d6d8d9cc
fuhuan.moyun <276727468@qq.com>
2021-08-06 14:39:19 +0800

    seconde commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var url = require('url');
+var util = require('util');
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,9 +16,16 @@ app.get('/', function (req, res) {
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
+        var params = url.parse(req.url, true).query;
+        console.log(params);
+        if (params['hub.verify_token'] === 'testbot_verify_token') {
+            res.send(params['hub.challenge']);
+        } else {
+            res.send('Invalid verify token');
+        }
+//    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+//        res.send(req.query['hub.challenge']);
+//    } else {
+//        res.send('Invalid verify token');
+//    }
 });
