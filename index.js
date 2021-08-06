985ce287721706c4d51cefb8e19098e22b870841
fuhuan.moyun <276727468@qq.com>
2021-08-06 17:45:17 +0800

    test

--- index.js
+++ index.js
@@ -17,80 +17,25 @@ app.get('/', function (req, res) {
 // Facebook Webhook
 app.post('/webhook', function (req, res) {
         console.log("post-welcome!");
-      var entry = req.body.entry;
-    console.log("entry--->"+JSON.stringify(entry));
-
-
-
-//
-//     var objArr= entry[0];
-//     objArr.forEach(function(obj, index) {
-//        Object.keys(obj).forEach(function(key){
-//                console.log(key,obj[key]);
-//
-//         });
-//     });
-
-
-
-//    var events = entry[0].messaging
-//    console.log("events--->"+events)
-//
-//      for (i = 0; i < events.length; i++) {
-//          var event = events[i];
-//          if (event.message && event.message.text) {
-//              console.log("text--->"+event.message.text);
-//          }
-//      }
         var params = url.parse(req.url, true).query;
         console.log(params);
         if (params['hub.verify_token'] === 'testbot_verify_token') {
             res.send(params['hub.challenge']);
         } else {
-            res.send('Invalid verify token');
+             var entry = req.body.entry;
+              console.log("entry--->"+JSON.stringify(entry));
+//            res.send('Invalid verify token');
         }
-//    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-//        res.send(req.query['hub.challenge']);
-//    } else {
-//        res.send('Invalid verify token');
-//    }
 });
 app.get('/webhook', function (req, res) {
         console.log("get-welcome!");
-//        console.log(req);
-
-          var entry = req.body.entry;
-          console.log("entry--->"+entry)
-          console.log("entry[0]--->"+entry[0])
-
-
-     var objArr= entry[0];
-     objArr.forEach(function(obj, index) {
-        Object.keys(obj).forEach(function(key){
-                console.log(key,obj[key]);
-
-         });
-     });
-          var events = entry[0].messaging
-          console.log("events--->"+events)
-
-            for (i = 0; i < events.length; i++) {
-                var event = events[i];
-                if (event.message && event.message.text) {
-                    console.log("text--->"+event.message.text);
-                }
-            }
-
         var params = url.parse(req.url, true).query;
         console.log(params);
         if (params['hub.verify_token'] === 'testbot_verify_token') {
             res.send(params['hub.challenge']);
         } else {
-            res.send('Invalid verify token');
+         var entry = req.body.entry;
+          console.log("entry--->"+JSON.stringify(entry));
+//           res.send('Invalid verify token');
         }
-//    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-//        res.send(req.query['hub.challenge']);
-//    } else {
-//        res.send('Invalid verify token');
-//    }
 });
